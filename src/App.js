cd81028f8c9f479117b21d2267f4579519233bb8
Alex Aguiñaga <marioalejandro300402@gmail.com>
2021-07-31 19:19:15 -0500

    Solucion de pequeño error

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import Slider from './componets/Slider'
 import MenuContainer from './componets/MenuContainer'
 import NavBar from './componets/NavBar';
-import { useState } from 'react';
 import Footer from './componets/Footer';
 import PromotionsContainer from './componets/PromotionsContainer';
 import Opinions from './componets/Opinions';
